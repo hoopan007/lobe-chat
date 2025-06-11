5688a4a06264c206e2e70330c6687bf53fdf2009
Hoopan <hongpanzhou@gmail.com>
2025-06-11 01:32:48 +0000

    m

--- src/app/api/slark-config/route.ts
+++ src/app/api/slark-config/route.ts
@@ -1,7 +1,5 @@
 import { NextResponse } from 'next/server';
 
-export const runtime = 'edge';
-
 export async function GET() {
   return NextResponse.json({
     NEXT_PUBLIC_SLARK_PATH_PRICING: process.env.NEXT_PUBLIC_SLARK_PATH_PRICING || '',
